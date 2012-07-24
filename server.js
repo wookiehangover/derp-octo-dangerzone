7b4c1b5874dcf133b50454f766d59eb3f45529b2
Sam Breed <sam@quickleft.com>
2012-07-24 12:38:53 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+var ecstatic = require('ecstatic');
+var ramrod = require('ramrod');
+
+var ec = ecstatic( __dirname );
+var router = ramrod();
+
+router.add("assets/*path", function(req, res){
+  ec( req, res );
+});
+
+router.add("app/*path", function(req, res){
+  ec( req, res );
+});
+
+router.on('*', function( req, res ){
+  fs.createReadStream('./index.html').pipe(res);
+});
+
+http.createServer(function( req, res ){
+  router.dispatch( req, res );
+}).listen(3000);
+
